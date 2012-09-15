cf7e9e784230e9ae7085a223bbdd60e666d01dfc
Owen Versteeg <owen.versteeg@live.com>
2012-09-15 01:29:41 -0300

    Fixed bug
    I was counting and adding too much...

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var fdom = new Date(new Date().getFullYear(), new Date().getMonth(), 1).getDay()+1; //finds first day of month
 
 function refreshStuff() {
-	for (var i=1; i<35; i++) {
-		document.getElementById('day'+i).childNodes[1].childNodes[0].innerText = ((i-fdom)+1); //labels days
+	for (var i=1; i<34; i++) {
+		document.getElementById('day'+i).childNodes[1].childNodes[0].innerText = ((i-fdom)); //labels days
 	}
 }
 
