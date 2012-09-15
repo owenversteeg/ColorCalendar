d4249d99c1685671426504b3d4d08b11663541d5
Owen Versteeg <owen.versteeg@live.com>
2012-09-14 22:37:23 -0400

    made js work (w/error)
    now it labels the dates sortof

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var fdom = new Date(new Date().getFullYear(), new Date().getMonth(), 1).getDay()+1; //finds first day of month
 
 function refreshStuff() {
-	document.getElementById('day'+fdom).childNodes[1].childNodes[0].innerText = '1'; //labels days
+	for (var i=1; i<35; i++) {
+		document.getElementById('day'+i).childNodes[1].childNodes[0].innerText = ((i-fdom)+1); //labels days
+	}
 }
 
 refreshStuff();
