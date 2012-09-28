9794ab0637296a0714439af988d22dddde12e4e2
Owen Versteeg <owen.versteeg@live.com>
2012-09-28 17:08:50 -0400

    added more data, css
    now things are better positioned

--- data.js
+++ data.js
@@ -4,9 +4,13 @@ var violin = {
 	'name': 'Violin',
 	'color': 'blue',
 	'9/11/2012': '08:00 Practiced for 15 minutes',
-	'9/12/2012': '09:00 Looked at violin case',
-	'9/14/2012': '20:00 Went to UVMC for lesson, practiced 20 minutes',
-	'8/31/2012': '15:00 Ate a cookie'
+	'9/14/2012': '17:30 Went to UVMC for lesson, practiced 20 minutes',
+	'9/15/2012': '17:00 Practiced',
+	'9/21/2012': '17:30 Went to UVMC for lesson',
+	'9/22/2012': '17:00 Practiced everything in the notebook',
+	'9/24/2012': '17:00 Practiced everything in the notebook',
+	'9/26/2012': '17:00 Practiced all from notebook, did vibrato exercises 2x',
+	'9/27/2012': '17:00 Practiced the things in the notebook'
 };
 
 var school = {
